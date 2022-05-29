40e6fde0f0bc13b0b4d98bd8f8f8ef2a7de3fb8a
djmatlas <djmdeveloper060796@gmail.com>
2022-05-29 16:48:35 +0530

    Add constants and data

--- cupcake/main.asm
+++ cupcake/main.asm
@@ -1,16 +1,30 @@
 
- 
+
+%include 'macros.asm'
+
 SECTION .text
 global  _start
  
 _start:
  
-    mov     edx, 13
-    mov     ecx, msg
-    mov     ebx, 1
-    mov     eax, 4
-    int     80h
+; setup listener socket
+_setup_socket:
+
+
 
 SECTION .data
-boot_up_message     db      'Starting cupcake on port 9200 ...', 0Ah     ; assign msg variable with your message string
+boot_up_message     db      'Starting cupcake on port 9200 ...', 0Ah, 0     ; assign msg variable with your message string
 boot_up_message_len equ $-boot_up_message
+socket_creation_message     db      'Creating socket ...', 0Ah, 0
+socket_creation_message_len     equ     $-socket_creation_message
+socket_bind_message     db      'Binding socket to IP:PORT ...', 0Ah, 0
+socket_bind_message_len     equ     $-socket_bind_message
+socket_listen_attempt_message       db      'Attempting to listen ...', 0Ah, 0
+socket_listen_attempt_message_len       equ     $-socket_listen_attempt_message
+socket_listening_message        db      'Cupcake is listenning for new connections ...'
+socket_listening_message_len        equ     $-socket_listening_message
+
+SECTION .bss
+response_buffer resb 512
+file_content_buffer 255
+
