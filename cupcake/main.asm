61877ba911ecc725f596a38e3b773f415e519839
djmatlas <djmdeveloper060796@gmail.com>
2022-06-12 17:26:41 +0530

    Update message

--- cupcake/main.asm
+++ cupcake/main.asm
@@ -17,7 +17,7 @@ sys_write_string boot_up_message, boot_up_message_len
 ; setup listener socket
 _setup_listener_socket:
 
-sys_write_string socket_creation_message, socket_bind_message_len
+sys_write_string socket_creation_message, socket_creation_message_len
 
 .create_socket:
     push byte 6
@@ -91,7 +91,7 @@ boot_up_message     db      'Starting cupcake on port 9001 ...', 0Ah, 0     ; as
 boot_up_message_len equ $-boot_up_message
 socket_creation_message     db      'Creating socket ...', 0Ah, 0
 socket_creation_message_len     equ     $-socket_creation_message
-socket_bind_message     db      'Binding socket to IP:PORT ...', 0Ah, 0
+socket_bind_message     db      'Binding socket to 0.0.0.0:9001 ...', 0Ah, 0
 socket_bind_message_len     equ     $-socket_bind_message
 socket_listen_attempt_message       db      'Attempting to listen ...', 0Ah, 0
 socket_listen_attempt_message_len       equ     $-socket_listen_attempt_message
