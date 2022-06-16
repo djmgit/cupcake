25670cd00e487fe89198b89319b91ca4440ec840
djmatlas <djmdeveloper060796@gmail.com>
2022-06-16 21:20:12 +0530

    Add inline doc

--- cupcake/http_request_parser.asm
+++ cupcake/http_request_parser.asm
@@ -1,19 +1,22 @@
 
-
+; Subroutine to process http request and extract http method
+; resource path, protocol and version into different memory locations
+; Usage: call process_http_request
+;        expects memory location of the first list of the http request in eax
 process_http_request:
-    push edx
+    push edx                                                ; save edx, ecx, and eax on stack
     push ecx
     push eax
-    mov edx, http_method
+    mov edx, http_method                                    ; move memory location of uninitialised http_method to edx
 
 .parse_http_method:
-    cmp byte [eax], 20h
-    jz .process_http_path
-    mov cl, byte [eax]
-    mov byte [edx], cl
-    inc edx
-    inc eax
-    jmp .parse_http_method
+    cmp byte [eax], 20h                                     ; check if we have encountered whitespace
+    jz .process_http_path                                   ; if we have, then we can start with processing the http path
+    mov cl, byte [eax]                                      ; move byte from request memory location to bl
+    mov byte [edx], cl                                      ; move byte from bl to memory location pointed by edx
+    inc edx                                                 ; increment edx
+    inc eax                                                 ; increment eax
+    jmp .parse_http_method                                  ; loop back
 
 .process_http_path:
     mov byte [edx], 0
