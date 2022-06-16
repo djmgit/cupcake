4bea47aa2ea079d349acac642cda9681852bef78
djmatlas <djmdeveloper060796@gmail.com>
2022-06-16 23:20:40 +0530

    Add inline doc

--- cupcake/file_handler.asm
+++ cupcake/file_handler.asm
@@ -1,28 +1,31 @@
 %include 'macros.asm'
 %include 'util.asm'
 
+; subroutine to read content from desired resource file
+; Usage: call generate_response_from_file
+;        expected the file path in eax
 generate_response_from_file:
-    push edx
+    push edx                                                ; save required registers on stack
     push ecx
     push ebx
     push eax
 
 .read_file:
-    mov ecx, 0
-    mov ebx, eax
-    mov eax, 5
-    int 80h
-    cmp eax, 0
-    jl .finished_response_generation
-    mov [fd_in], eax
-    mov dword [bytecount], dword 0h
+    mov ecx, 0                                              ; O_RDONLY file access mode in ecx
+    mov ebx, eax                                            ; move file path to ebx
+    mov eax, 5                                              ; move sys_open syscall number to eax
+    int 80h                                                 ; invoke kernel
+    cmp eax, 0                                              ; check for errors
+    jl .finished_response_generation                        ; if there is an error, that is, eax value is less than 0 then quit
+    mov [fd_in], eax                                        ; if no error then we have the fd in eax, move it to dedicated memory location
+    mov dword [bytecount], dword 0h                         ; move 0 to our offset counter
 
 .set_seek_offset:
-    mov edx, 0
-    mov ecx, [bytecount]
-    mov ebx, [fd_in]
-    mov eax, 19
-    int 80h
+    mov edx, 0                                              ; we want to set the offset from the beginning of the file hence 0
+    mov ecx, [bytecount]                                    ; the offset value in ecx
+    mov ebx, [fd_in]                                        ; fd in ebx
+    mov eax, 19                                             ; sys_lseek syscall number in eax
+    int 80h                                                 ; invoke kernel
 
 .read_byte:
     mov edx, 1
