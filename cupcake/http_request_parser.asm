77a25e659220a07bc5ec4632584cce508f910258
djmatlas <djmdeveloper060796@gmail.com>
2022-06-16 23:07:04 +0530

    Add inline doc

--- cupcake/http_request_parser.asm
+++ cupcake/http_request_parser.asm
@@ -18,50 +18,50 @@ process_http_request:
     inc eax                                                 ; increment eax
     jmp .parse_http_method                                  ; loop back
 
-.process_http_path:
-    mov byte [edx], 0
-    mov edx, http_path
-    inc eax
+.process_http_path:                                         ; start with processing the http path
+    mov byte [edx], 0                                       ; null terminate the http method
+    mov edx, http_path                                      ; move the http_path uninitialised memory location to edx
+    inc eax                                                 ; increment eax to go beyond the whitespace
 
-.parse_http_path:
-    cmp byte [eax], 20h
-    jz .process_http_protocol
-    mov cl, byte [eax]
+.parse_http_path:                                           ; start with parsing the http path
+    cmp byte [eax], 20h                                     ; check if we have encountered whitespace
+    jz .process_http_protocol                               ; if we have then start with protocol processing
+    mov cl, byte [eax]                                      ; same as before copy the http_path from the request buffer to its desired memory location
     mov byte [edx], cl
     inc edx
     inc eax
-    jmp .parse_http_path
+    jmp .parse_http_path                                    ; loop
 
-.process_http_protocol:
+.process_http_protocol:                                     ; process and parse the http protocol in similar way as above
     mov byte [edx], 0
     mov edx, http_protocol
     inc eax
 
 .parse_http_protocol:
-    cmp byte [eax], 2Fh
+    cmp byte [eax], 2Fh                                     ; this time we look out for 2Fh which is the hex for '/' because the format is protocol/version
     jz .process_http_version
     mov cl, byte [eax]
     mov byte [edx], cl
     inc edx
     inc eax
-    jmp .parse_http_protocol
+    jmp .parse_http_protocol                                ; loop
 
-.process_http_version:
+.process_http_version:                                      ; finally we extract the http version into its memory location from the request buffer
     mov byte [edx], 0
     mov edx, http_version
     inc eax
 
 .parse_http_version:
-    cmp byte [eax], 0Ah
+    cmp byte [eax], 0Ah                                     ; For version parsing, we look for 0Ah which is the hex for new line
     jz .http_request_processing_finished
     mov cl, byte [eax]
     mov byte [edx], cl
     inc edx
     inc eax
-    jmp .parse_http_version
+    jmp .parse_http_version                                 ; loop
 
 .http_request_processing_finished:
-    pop eax
+    pop eax                                                 ; restore the saved registers
     pop ecx
     pop edx
-    ret
+    ret                                                     ; return
